024e9cf1c52f1ec0b5637469513ea41f6bedf142
Повстяной Кирилл <KPovstyanoy@medlinesoft.ru>
2026-08-04 15:44:53 +0300

    rt7

--- play/flutter_bootstrap.js
+++ play/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"77e2e94772b6eb43759e34ed1ad7da4674e19c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2255258740" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3880307051" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
